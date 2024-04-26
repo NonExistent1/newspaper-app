927b36225f0182430ca3d033327e43de0b658142
Jordyn <61482230+NonExistent1@users.noreply.github.com>
2024-04-26 08:54:36 -0400

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -147,7 +147,7 @@ LOGOUT_REDIRECT_URL = "home"
 # Email Settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = "noreply@localhost"
-EMAIL_HOST = "barnesbrothers.net"
+EMAIL_HOST = ""
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 2500
